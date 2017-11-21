70d95aecff1a553e734e0405732b2f51b27b5d9d
Jorge Bolpe <ygd182@gmail.com>
2017-11-21 13:32:59 -0300

    Update services.js

--- app/js/services.js
+++ app/js/services.js
@@ -5,7 +5,7 @@
 angular.module('App').service('TestService', ['$http',
   function TestService($http){
   	this.getData = function getData () {
-  		return $http.get('http://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=b1b15e88fa797225412429c1c50c122a1');
+  		return $http.get('https://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=b1b15e88fa797225412429c1c50c122a1');
   	}
 
 }]);
